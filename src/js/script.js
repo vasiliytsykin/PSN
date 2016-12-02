3e483c7f2576bb95a26647b65d6525d52f36224e
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-12-02 18:58:20 +0500

    main page almost done

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,63 @@
-function attachActiveClass(element) {
-    var active = 'active'
-    $(".mobile-nav-header").removeClass(active);
-    if(!$(element).hasClass(active) && $(element).hasClass('on'))
-        $(element).addClass(active)
+// function attachActiveClass(element) {
+//     var active = 'active'
+//     $(".mobile-nav-header").removeClass(active);
+//     if(!$(element).hasClass(active) && $(element).hasClass('on'))
+//         $(element).addClass(active)
+// }
+//
+// $(document).ready(function () {
+//     $(".mobile-nav-header").click(function() {
+//
+//         if($(this).hasClass('main') && $(this).hasClass('on'))
+//         {
+//             $(".mobile-nav-header").removeClass('on')
+//             $('.mobile-nav ul').slideUp();
+//         }
+//         else
+//         {
+//             $(this).toggleClass('on');
+//
+//             var parent = $(this).closest('.mobile-nav');
+//             var ul = $(parent).find('ul:first');
+//
+//             $(ul).slideToggle();
+//         }
+//
+//         attachActiveClass(this);
+//
+//         return false;
+//     });
+// });
+
+function engageCarousel(element, settings) {
+    
+    element.addClass('owl-carousel').owlCarousel(settings);
+    
 }
 
-$(document).ready(function () {
-    $(".mobile-nav-header").click(function() {
+$(function () {
 
-        if($(this).hasClass('main') && $(this).hasClass('on'))
-        {
-            $(".mobile-nav-header").removeClass('on')
-            $('.mobile-nav ul').slideUp();
-        }
-        else
-        {
-            $(this).toggleClass('on');
 
-            var parent = $(this).closest('.mobile-nav');
-            var ul = $(parent).find('ul:first');
+    $('.burger-btn').on('click', function () {
 
-            $(ul).slideToggle();
-        }
+        $(this).closest('.header__nav').toggleClass('open');
+        $('.top-level').removeClass('open');
 
-        attachActiveClass(this);
-
-        return false;
     });
+
+    $('.header__nav .nested').on('click', function () {
+
+        $(this).closest('.top-level').toggleClass('open');
+
+    });
+
+    engageCarousel($('.main-slider'), {
+        items: 1,
+        loop: false,
+        nav: true,
+        navText: ['', '']
+    });
+    
 });
 
 
