06e38155756582016929b5cf72f4355040160134
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-12-15 14:59:03 +0500

    plan-filter-detail page started

---
+++ src/plan-filter-detail.php
@@ -0,0 +1,5 @@
+<? include 'include/header.php'?>
+    <div class="plan-filter-detail-page">
+        <h1 class="dark-green">ТИП 1.1-А <span>евроформат</span></h1>
+    </div>
+<? include 'include/footer.php'?>
